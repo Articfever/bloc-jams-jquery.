ca35d37171eaa0207b4ea638bd5532b3a1c78925
rachael <raexcore@gmail.com>
2018-01-20 11:06:59 -0800

    please work

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,7 @@
 {
 $('#album-title').text(album.title);
 $('img#album-cover-art').attr('src', album.albumArtURL);
+$('album-artist').text(album.artist);
+$('album-release-info').text(album.release-info);
+
 }
